94bf54e7b0926d44329fae51e608411e958d1f86
Soaryoto <danilatt609@gmail.com>
2021-04-21 21:04:45 +0500

    Add new functions in hw1

--- Homework/hw1/main.py
+++ Homework/hw1/main.py
@@ -1,5 +1,45 @@
 # Напечатал Danila Dumbrovskiy Ivanovich
 
+# Простые переменки нужные для процесса
+
+g_u_answer = ["да", "yes", "ok", "ок"]
+b_indicator_dullness = 0
+
+img = ''',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,r,:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+,,,,,,,,,,,,,,,,,,,,,,,,,,,@@@@,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+,,,,,,,,,,,,,,,,,,,,,,,,:@@:,,@@,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+,,,,,,,,,,,,,,,,,,,,:M@@@,,,,::@M,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+,,,,,,,,,,,,,,,,,:@@@:,,,,,,:,,:2@@@@@H:,,,,,,,,,,,,,,,,,,,,,,,,
+,,,,,,,,,,,,,,,@@9:,,,,,,,,,,,,,,,,,,,,S@@@i,,,,,,,,,,,,,,,,,,,,
+,,,A@@r,,,,,:@@,,,,,,,,,,,,,,,,,,,,,,,,,,,,:@@@,,,,,,,,,,,,,,,,,
+,@@,,,r@@,;@@,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,#@@:,,,,,,,,B2,,,
+@@,:s,i;@@@:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,:@@:,,X@@BM@,,,
+@:r:ss;:,HM:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,@@H:,,,@@,,,
+@,,ssis:,,,,,,,,,,X@@,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,@;,,,
+@:,:rr,,,,,,,,,,,:@@@:,,@,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,&B,,,,
+@,,,,,,,,,,,,,,,,,,,:,,r@:,@#:,,,,,,,,,,,,,,,2@@X,,,,,,,,,,@,,,,
+@G,,,,,,,,,,,,,,,,,,,,,,:GBA@A@@,,,,,,,,,,,A@;,,r@@,,,,,,,s@,,,,
+G@@@@@B;,,,,,,,,,,,,,,,,,,,,,:::,,,,,,#@H,;@:,s:rs@@,,,,,,#@,,,,
+,,,,,,:5@@@@@@S,,,,,,,,,,,,,,,,,,,,,,,@@@,@3r;;ir:,G&,,,,:@@:,,,
+,,,,,,,,,,,,,,s@@@@@@3::,,,,,,,,,,,,,,,,,:@:,:siis,,s:,,,,,;@;,,
+,,,,,,,,,,,,,,,,,,,::G@@@@@#:,,,,,,,,,,,,:@;,,;sr,,,,,,,,,,,r@:,
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,5@@@@@@X,,,,,,@X,,,,,,,,,,,,,,,,,@@,
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,rM@@@@@G:,,,,,,,,,,,,,,,,,,@:
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,G@@@@@@;,,,,,,,,,,,@3
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,5@@@@@@5,,,,,@M
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,sA@@@@@M
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'''
+
+# Вступлени
+
+print(img)
+print()
+print("Привет, сегодня я буду работать с текстом который вы будете мне давать")
+print()
+
+# /Вступление
+
 # Задание 1 - без разници, есть такое слово или нет
 
     
@@ -15,14 +55,75 @@ c = temp_arr[int(len(temp_arr) / 4 + len(temp_arr) / 2)]
 d = temp_arr[len(temp_arr) -1]
 i = temp_arr[int(len(temp_arr) / 4)]
 
-print((d+i+c+b+a+d).capitalize(), c+d+a+i)
+print()
+print("Я создал новые слова: ",(d+i+c+b+a+d).capitalize(), c+d+a+i)
 
 print("Во, я самый грамотный бот на свете")
 
 # /Задание 1 - Что там дальше?
 
+# Задание 1.5 - это моё задание, дополнение
+print()
+print("У меня есть некая база знаний слов, хочешь узнать какие слова подойдут под эти буквы ?")
+u_t_sentence_for_new_sentence = input()
+
+if u_t_sentence_for_new_sentence.lower() in g_u_answer:
+    b_notable_words = ["привет", "как", "дела", "лол", "магия", "прогер", "автоэлектростеклоподъемник"]
+    b_answer_q_1_5 = ""
+
+    temp_t = b_notable_words[0] # В условии не хочу использовать длинную переменную
+    if temp_t[0] in temp_arr and temp_t[1] in temp_arr and temp_t[2] in temp_arr and temp_t[3] in temp_arr and temp_t[4] in temp_arr and temp_t[5] in temp_arr:
+        b_answer_q_1_5 = b_answer_q_1_5 + temp_t
+
+    temp_t = b_notable_words[1]
+    if temp_t[0] in temp_arr and temp_t[1] in temp_arr:
+        if b_answer_q_1_5 == "":
+            b_answer_q_1_5 = b_answer_q_1_5 + temp_t
+        else:
+            b_answer_q_1_5 = b_answer_q_1_5 + " " + temp_t
+        
+    temp_t = b_notable_words[2]
+    if temp_t[0] in temp_arr and temp_t[1] in temp_arr and temp_t[2] in temp_arr and temp_t[3] in temp_arr:
+        if b_answer_q_1_5 == "":
+            b_answer_q_1_5 = b_answer_q_1_5 + temp_t
+        else:
+            b_answer_q_1_5 = b_answer_q_1_5 + " " + temp_t
+
+    temp_t = b_notable_words[3]
+    if temp_t[0] in temp_arr and temp_t[1] in temp_arr:
+        if b_answer_q_1_5 == "":
+            b_answer_q_1_5 = b_answer_q_1_5 + temp_t
+        else:
+            b_answer_q_1_5 = b_answer_q_1_5 + " " + temp_t
+        
+    temp_t = b_notable_words[4]
+    if temp_t[0] in temp_arr and temp_t[1] in temp_arr and temp_t[2] in temp_arr and temp_t[3] in temp_arr and temp_t[4] in temp_arr:
+        if b_answer_q_1_5 == "":
+            b_answer_q_1_5 = b_answer_q_1_5 + temp_t
+        else:
+            b_answer_q_1_5 = b_answer_q_1_5 + " " + temp_t
+
+    temp_t = b_notable_words[5]
+    if temp_t[0] in temp_arr and temp_t[1] in temp_arr and temp_t[2] in temp_arr and temp_t[3] in temp_arr and temp_t[4] in temp_arr and temp_t[5] in temp_arr:
+        if b_answer_q_1_5 == "":
+            b_answer_q_1_5 = b_answer_q_1_5 + temp_t
+        else:
+            b_answer_q_1_5 = b_answer_q_1_5 + " " + temp_t
+
+    if b_answer_q_1_5 == "":
+        print("Ой, я не знаю ни одного слово из этих букв")
+    elif len(b_answer_q_1_5.split(" ")) == 1:
+        print("Я знаю только одно слово '{}' из этих букв".format(b_answer_q_1_5))
+    else:
+        print("Отлично я знаю парочку слов: ", b_answer_q_1_5)
+else:
+    b_indicator_dullness = b_indicator_dullness + 1
+# /Задание 1.5
+
 # Задание 2 - Ща сделаем
 
+print()
+print("Сейчас я буду пытаться заменить часть строки в предложении")
 print("Введите предложение: ")
 u_t_sentence_for_replase = input()
 
@@ -32,6 +133,7 @@ u_t_world_for_replase = input()
 print("А на что вы хотите заменить?")
 u_t_world_to_replase = input()
 
+print()
 bot_t_answer_for_replase = "Я заменил слово, и теперь получилось такое предложение :\n{}"
 print(
     bot_t_answer_for_replase.format(
@@ -44,6 +146,7 @@ print(
 # Задание 3 - Не делать, так там требуется цикл котрый мы не учили
 
 # Задание 4 - Так а тут сказали что только 4 слова в предложении, значит делаем без цыклов)
+print()
 print("Введите предложение из 4 слов и я определю какое из слов большое:")
 u_t_sentence_for_find_max_world = input()
 
