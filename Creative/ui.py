bb5ec345bc4fd530f9b5ebd55e8f9db3b2d1e3f9
Soaryoto <danilatt609@gmail.com>
2021-05-01 01:33:58 +0500

    I am tired

--- Creative/ui.py
+++ Creative/ui.py
@@ -19,7 +19,9 @@ class Ui_Form(object):
             Form.setObjectName(u"Form")
         Form.resize(514, 397)
         Form.setStyleSheet(u"QWidget { \n"
-"	background-color: red;\n"
+"	background-color: black;\n"
+"	border-color: white;\n"
+"	color: white;\n"
 "}\n"
 "QVBoxLayout {\n"
 "	margin:  20px;\n"
@@ -29,16 +31,25 @@ class Ui_Form(object):
         self.verticalLayoutWidget.setGeometry(QRect(10, 10, 381, 301))
         self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setSizeConstraint(QLayout.SetMaximumSize)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.label = QLabel(self.verticalLayoutWidget)
         self.label.setObjectName(u"label")
+        self.label.setMinimumSize(QSize(200, 0))
         self.label.setLineWidth(1)
-        self.label.setMidLineWidth(0)
+        self.label.setMidLineWidth(1)
 
         self.horizontalLayout.addWidget(self.label)
+
+        self.plainTextEdit = QPlainTextEdit(self.verticalLayoutWidget)
+        self.plainTextEdit.setObjectName(u"plainTextEdit")
+        self.plainTextEdit.viewport().setProperty("cursor", QCursor(Qt.IBeamCursor))
+        self.plainTextEdit.setMidLineWidth(1)
+
+        self.horizontalLayout.addWidget(self.plainTextEdit)
 
 
         self.verticalLayout.addLayout(self.horizontalLayout)
@@ -55,7 +66,7 @@ class Ui_Form(object):
     # setupUi
 
     def retranslateUi(self, Form):
-        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"This is comment", None))
         self.label.setText("")
     # retranslateUi
 
