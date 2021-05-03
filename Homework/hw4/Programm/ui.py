8dd4ef65c6ccb9e4cf3551f6e3357a036b830b9e
Soaryoto <danilatt609@gmail.com>
2021-05-03 21:44:59 +0500

    New UI

--- Homework/hw4/Programm/ui.py
+++ Homework/hw4/Programm/ui.py
@@ -17,48 +17,65 @@ class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(514, 397)
-        Form.setStyleSheet(u"QWidget { \n"
-"	background-color: black;\n"
-"	border-color: white;\n"
-"	color: white;\n"
-"}\n"
-"QVBoxLayout {\n"
-"	margin:  20px;\n"
-"}")
+        Form.resize(479, 526)
+        Form.setStyleSheet(u"")
         self.verticalLayoutWidget = QWidget(Form)
         self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(10, 10, 381, 301))
+        self.verticalLayoutWidget.setGeometry(QRect(20, 10, 431, 501))
         self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setSizeConstraint(QLayout.SetMaximumSize)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.listView = QListView(self.verticalLayoutWidget)
+        self.listView.setObjectName(u"listView")
+        self.listView.setStyleSheet(u"QListView {\n"
+"	background-color: rgba(255, 255, 255, 0.5);\n"
+"}")
+
+        self.verticalLayout.addWidget(self.listView)
+
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.label = QLabel(self.verticalLayoutWidget)
-        self.label.setObjectName(u"label")
-        self.label.setMinimumSize(QSize(200, 0))
-        self.label.setLineWidth(1)
-        self.label.setMidLineWidth(1)
+        self.horizontalLayout.setSizeConstraint(QLayout.SetDefaultConstraint)
+        self.lineEdit = QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setStyleSheet(u"QLineEdit {\n"
+"	background-color: rgba(255, 255, 255, 0.5);\n"
+"	color: black;\n"
+"}")
 
-        self.horizontalLayout.addWidget(self.label)
+        self.horizontalLayout.addWidget(self.lineEdit)
 
-        self.plainTextEdit = QPlainTextEdit(self.verticalLayoutWidget)
-        self.plainTextEdit.setObjectName(u"plainTextEdit")
-        self.plainTextEdit.viewport().setProperty("cursor", QCursor(Qt.IBeamCursor))
-        self.plainTextEdit.setMidLineWidth(1)
+        self.pushButton = QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setCursor(QCursor(Qt.PointingHandCursor))
+        self.pushButton.setStyleSheet(u"QPushButton {\n"
+"	background-color: rgba(255,255,255, 0);\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"	background-color: rgba(128, 166, 255, 0.5);\n"
+"}")
+        icon = QIcon()
+        icon.addFile(u"icons/send.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton.setIcon(icon)
 
-        self.horizontalLayout.addWidget(self.plainTextEdit)
+        self.horizontalLayout.addWidget(self.pushButton)
 
 
         self.verticalLayout.addLayout(self.horizontalLayout)
 
-        self.lineEdit = QLineEdit(self.verticalLayoutWidget)
-        self.lineEdit.setObjectName(u"lineEdit")
-
-        self.verticalLayout.addWidget(self.lineEdit)
-
+        self.label = QLabel(Form)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(0, 0, 481, 531))
+        self.label.setStyleSheet(u"QLabel {\n"
+"	background-image: url(\"images/background.jpg\");\n"
+"	background-repeat: no-repeat;\n"
+"	background-attachment: fixed;\n"
+"	background-position: center;\n"
+"}")
+        self.label.raise_()
+        self.verticalLayoutWidget.raise_()
 
         self.retranslateUi(Form)
 
@@ -66,7 +83,8 @@ class Ui_Form(object):
     # setupUi
 
     def retranslateUi(self, Form):
-        Form.setWindowTitle(QCoreApplication.translate("Form", u"This is comment", None))
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Telegram v0.1", None))
+        self.pushButton.setText("")
         self.label.setText("")
     # retranslateUi
 
